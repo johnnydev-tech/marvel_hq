913e1a35ca087d24fa39dfe8364515a7ad23dfa3
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-02-06 01:05:16 -0300

    Home with API and styled

--- lib/model/comic.dart
+++ lib/model/comic.dart
@@ -1,12 +1,14 @@
 class Comic{
   String thumb;
   String extension;
+  String title;
 
-  Comic({this.thumb, this.extension });
+  Comic({this.title, this.thumb, this.extension });
 
   factory Comic.fromJsom(Map<String, dynamic> json) {
     return Comic(
       thumb: json["thumbnail"]["path"],
+      title: json["title"],
       extension: json["thumbnail"]["extension"],
     );
   }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -29,62 +29,127 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("MARVEL"),
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 35,
+        ),
         centerTitle: true,
       ),
       body: Container(
         height: _tela.height,
         width: _tela.width,
-        child: FutureBuilder(
-          future: ComicsGet(),
-          builder: (context, snapshot) {
-            // ignore: missing_return
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return Center(
-                  child: Text("Sem Conexão"),
-                );
-              case ConnectionState.waiting:
-                return Center(
-                  child: CircularProgressIndicator(
-                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
-                  ),
-                );
-                break;
-              case ConnectionState.active:
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Container(
-                    alignment: Alignment.center,
-                    child: Text("ERROR"),
-                  );
-                } else {
-                  return GridView.builder(
-                      shrinkWrap: true,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      scrollDirection: Axis.vertical,
-                      physics: ScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        childAspectRatio: 1,
+        child: ListView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "Veja Alguns Titulos de HQ's",
+                softWrap: true,
+                style: TextStyle(fontSize: 22, color: Colors.white),
+              ),
+            ),
+            FutureBuilder(
+              future: ComicsGet(),
+              builder: (context, snapshot) {
+                // ignore: missing_return
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return Center(
+                      child: Text("Sem Conexão"),
+                    );
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.red),
                       ),
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (context, index) {
+                    );
+                    break;
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Container(
+                        alignment: Alignment.center,
+                        child: Text("ERROR"),
+                      );
+                    } else {
+                      return GridView.builder(
+                          shrinkWrap: true,
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                          scrollDirection: Axis.vertical,
+                          physics: ScrollPhysics(),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: .55,
+                          ),
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            List<Comic> lista = snapshot.data;
+                            Comic comic = lista[index];
 
-                        List<Comic> lista = snapshot.data;
-                        Comic comic = lista[index];
-
-                        return Container(
-                          child: Image.network("${comic.thumb}.${comic.extension}"),
-                        );
-                      });
+                            return Container(
+                              child: InkWell(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(2),
+                                  topRight: Radius.circular(2),
+                                ),
+                                splashColor: Colors.red.withOpacity(.5),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Expanded(
+                                      child: Ink(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(2),
+                                            topRight: Radius.circular(2),
+                                          ),
+                                          image: DecorationImage(
+                                            image: NetworkImage(
+                                                "${comic.thumb}.${comic.extension}"),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 8, horizontal: 5),
+                                        height: 70,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white10,
+                                          borderRadius: BorderRadius.only(
+                                            bottomLeft: Radius.circular(2),
+                                            bottomRight: Radius.circular(2),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          comic.title,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ))
+                                  ],
+                                ),
+                                onTap: () {},
+                              ),
+                            );
+                          });
+                    }
+                    break;
                 }
-                break;
-            }
-          },
+              },
+            ),
+          ],
         ),
       ),
     );
